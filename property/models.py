7a997682e7bb8dc7a14a50069b01e15d1dad01a3
Hetzel Cordoba <hetzel30@gmail.com>
2021-04-23 16:16:42 -0500

    property model

---
+++ property/models.py
@@ -0,0 +1,12 @@
+import uuid
+
+from django.db import models
+
+
+class Property(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    catastro_id = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    solana_id = models.CharField(max_length=100, null=True, blank=True, unique=True)
+
+    date_created = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
