1b7a1b0b3fa6dbc3de66be929a83aeeeedd845cd
Hetzel Cordoba <hetzel30@gmail.com>
2021-04-24 10:45:14 -0500

    create token based on system wallet

--- property/admin.py
+++ property/admin.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+
 from django.contrib import admin
 
 from property.models import Property
@@ -5,8 +8,32 @@ from property.models import Property
 
 @admin.action(description='Generate Solana id')
 def generate_solana_id_admin(modeladmin, request, queryset):
-    for property in queryset:
-        print(property.id)
+    for property_obj in queryset:
+        # output = os.system("spl-token create-token")
+        create_token = subprocess.getstatusoutput(f'spl-token create-token')
+        token_id = create_token[1].split(" ")[2].split("\n")[0]
+        create_account = subprocess.getstatusoutput(f'spl-token create-account {token_id}')
+        account_id = create_account[1].split("\n")[0].split(" ")[2]
+        mint_token = subprocess.getstatusoutput(f'spl-token mint {token_id} 1 {account_id}')
+        disable_mint_token = subprocess.getstatusoutput(f'spl-token authorize {token_id} mint --disable')
+        property_obj.solana_id = token_id
+        property_obj.save()
+        print(property_obj.id)
+
+
+@admin.action(description='Generate Solana id')
+def generate_solana_id_admin(modeladmin, request, queryset):
+    for property_obj in queryset:
+        # output = os.system("spl-token create-token")
+        create_token = subprocess.getstatusoutput(f'spl-token create-token')
+        token_id = create_token[1].split(" ")[2].split("\n")[0]
+        create_account = subprocess.getstatusoutput(f'spl-token create-account {token_id}')
+        account_id = create_account[1].split("\n")[0].split(" ")[2]
+        mint_token = subprocess.getstatusoutput(f'spl-token mint {token_id} 1 {account_id}')
+        disable_mint_token = subprocess.getstatusoutput(f'spl-token authorize {token_id} mint --disable')
+        property_obj.solana_id = token_id
+        property_obj.save()
+        print(property_obj.id)
 
 
 @admin.register(Property)
