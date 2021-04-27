c8e109882648f3dbb4efc8688616f7e9755758ef
Hetzel Cordoba <hetzel30@gmail.com>
2021-04-26 21:43:33 -0500

    search nested properties

--- property/views.py
+++ property/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.generics import ListAPIView, CreateAPIView
 
 from property.models import Property
@@ -8,8 +9,18 @@ from property.serializers import PropertySimpleSerializer
 class PropertyListView(ListAPIView):
     queryset = Property.objects.all()
     serializer_class = PropertySimpleSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['catastro_id', 'id', 'solana_id', 'ref_collection', 'form_id', ]
+    search_fields = [
+        'contributor__name',
+        'contributor__identification',
+        'contributor__type',
+        'contributor__property',
+        'contributor__quality',
+        'contributor__address',
+        'contributor__city',
+        'identification',
+    ]
 
 
 class PropertyCreateView(CreateAPIView):
